789ab1dd8340677fabd199c2de0d3a3285687602
shiva18christ <ratnashiva.sing@mscsa.christuniversity.in>
2025-02-28 16:16:25 +0530

    Another slight change

--- PrimeNo.java
+++ PrimeNo.java
@@ -10,6 +10,7 @@ public class PrimeNo {
                 break;
             } else if (number%i!=0) {
                 System.out.println("prime");
+                System.out.println("Proved")
                 break;
 
 
